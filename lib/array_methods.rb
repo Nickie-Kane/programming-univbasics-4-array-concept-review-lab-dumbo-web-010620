cf49c161343952d5a23c05a57ab95312d00497f2
nickie kane <kameo712@gmail.com>
2019-12-26 16:16:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,4 +1,8 @@
-array = [1,2,1,3,4,3,5,4,3,2,1]
+scale = [2,3,4,5]
+scale_2 = [6,5,4,3]
+hill = [1,2,3,4,5,4,3,2,1]
+valley = [5,4,3,2,1,0,1,2,3,4,5]
+zig_zag = [500,4,1000,5,250]
 
 def find_element_index(array, value_to_find)
     array.each do |v|
@@ -9,7 +13,7 @@ def find_element_index(array, value_to_find)
     
 end
 
-find_element_index(array, 5)
+find_element_index(scale, 2)
 
 def find_max_value(array)
   # Add your solution here
